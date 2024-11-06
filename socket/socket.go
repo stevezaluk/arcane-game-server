4faa25fd44f7d46f4b5f0b6d18ef3b35927632cc
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-06 14:43:04 -0500

    Implemented basiic support for starting listening on the socket

--- socket/socket.go
+++ socket/socket.go
@@ -1,1 +1,23 @@
 package socket
+
+import (
+	"net"
+
+	"github.com/spf13/viper"
+)
+
+type ServerSocket struct {
+	Listener *net.Listener
+}
+
+func (socket *ServerSocket) Start() {
+	uri := "localhost:" + viper.GetString("port")
+
+	listen, err := net.Listen("tcp", uri)
+	if err != nil {
+		// log here
+		panic(err) // panicing here as this is a fatal error
+	}
+
+	socket.Listener = &listen
+}
