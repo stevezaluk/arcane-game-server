3b3af67976f13c9c8136e5fa7c484cddbb7564b9
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-07 09:55:33 -0500

    Implemented support for generating private and public RSA key pairs. A 4096 bit key will be used for this

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,2 +1,17 @@
 package crypto
 
+import (
+	"crypto/rand"
+	"crypto/rsa"
+)
+
+func GenerateKeyPair() (rsa.PrivateKey, rsa.PublicKey) {
+	privateKey, err := rsa.GenerateKey(rand.Reader, 4096)
+	if err != nil {
+		panic(err) // panicing here as this is a fatal error
+	}
+
+	publicKey := privateKey.PublicKey
+
+	return *privateKey, publicKey
+}
