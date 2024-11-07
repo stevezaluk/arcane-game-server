860c2e4060e0ba0df229aa7a5bd34f853ad63d0a
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-06 19:49:40 -0500

    Implemented initial support for connecting to the socket. A max  connection count is enforced to ensure that the socket cant handle too many connections

--- socket/socket.go
+++ socket/socket.go
@@ -1,17 +1,21 @@
 package socket
 
 import (
+	"fmt"
 	"net"
 
 	"github.com/spf13/viper"
 )
 
 type Socket struct {
-	Listener *net.Listener
+	Listener        *net.Listener
+	ConnectionCount int
+	MaxConnections  int
+	IsClosed        bool
 }
 
 func (socket *Socket) Start() {
-	uri := "localhost:" + viper.GetString("port")
+	uri := "127.0.0.1:" + viper.GetString("port")
 
 	listen, err := net.Listen("tcp", uri)
 	if err != nil {
@@ -22,6 +26,42 @@ func (socket *Socket) Start() {
 	socket.Listener = &listen
 }
 
-func (socket *Socket) WaitForConnections() {
+func (socket *Socket) AcceptConnections() {
+	for {
+		if socket.ConnectionCount == socket.MaxConnections {
+			socket.IsClosed = true
+			break
+		}
 
+		if !socket.IsClosed {
+			sock := *socket.Listener
+			conn, err := sock.Accept()
+			if err != nil {
+				// log here
+			}
+
+			socket.ConnectionCount += 1
+			go socket.HandleClient(conn)
+		}
+	}
+}
+
+func (socket *Socket) HandleClient(conn net.Conn) {
+	for {
+		buffer := make([]byte, 4096)
+		_, err := conn.Read(buffer)
+		if err != nil {
+			// log here
+			// better error checking here
+			continue
+		}
+
+		fmt.Println("client msg: ", string(buffer))
+
+	}
+}
+
+func (socket *Socket) Stop() {
+	sock := *socket.Listener
+	sock.Close()
 }
