9126bd63527f61ba8a15353675a4fa8ad88a5ed1
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-08 10:41:25 -0500

    Implemented basic logging for server side connections

--- server/server.go
+++ server/server.go
@@ -5,9 +5,13 @@ import (
 	"crypto/rsa"
 	"encoding/base64"
 	"fmt"
+	"log/slog"
 	"net"
+	"os"
 	"strings"
+	"time"
 
+	"github.com/samber/slog-multi"
 	"github.com/spf13/viper"
 	"github.com/stevezaluk/arcane-game-server/crypto"
 )
@@ -21,11 +25,32 @@ type GameServer struct {
 
 	privateKey *rsa.PrivateKey
 	publicKey  rsa.PublicKey
+
+	Logger *slog.Logger
+}
+
+func (server *GameServer) initLogger() {
+	timestamp := time.Now().Format(time.RFC3339Nano)
+
+	filename := viper.GetString("log.path") + "/arcane-" + timestamp + ".json"
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	multiHandler := slogmulti.Fanout(
+		slog.NewJSONHandler(file, nil),
+		slog.NewTextHandler(os.Stdout, nil),
+	)
+
+	server.Logger = slog.New(multiHandler)
+	slog.SetDefault(server.Logger)
 }
 
 func (server *GameServer) Init() {
-	// init logger here
+	server.initLogger()
 
+	slog.Info("Generating RSA-4096 key pair...")
 	priv, _ := crypto.GenerateKeyPair()
 	server.privateKey = &priv
 	server.publicKey = server.privateKey.PublicKey
@@ -35,12 +60,14 @@ func (server *GameServer) Init() {
 }
 
 func (server *GameServer) Listen() {
+	slog.Info("Starting server...")
 	listen, err := net.Listen("tcp", server.URI)
 	if err != nil {
-		// log here
+		slog.Error("Failed to start listening for connections", "err", err.Error())
 		panic(err) // panicing here as this is a fatal error
 	}
 
+	slog.Info("Server listening for connections at", "uri", server.URI)
 	server.Listener = &listen
 }
 
@@ -48,6 +75,7 @@ func (server *GameServer) AcceptConnections() {
 	for {
 		if server.ConnectionCount == server.MaxConnections {
 			server.IsClosed = true
+			slog.Warn("Server reached max connection count. No new connections are accepted")
 			break
 		}
 
@@ -55,9 +83,11 @@ func (server *GameServer) AcceptConnections() {
 			sock := *server.Listener
 			conn, err := sock.Accept()
 			if err != nil {
-				// log here
+				slog.Error("Failed to accept connection from client ", "client", conn.RemoteAddr().String(), "err", err.Error())
+				panic(err)
 			}
 
+			slog.Info("Accepted connection from client", "client", conn.RemoteAddr().String())
 			server.ConnectionCount += 1
 			go server.NegotiateKeys(conn)
 		}
@@ -65,16 +95,14 @@ func (server *GameServer) AcceptConnections() {
 }
 
 func (server *GameServer) HandleClient(conn net.Conn) {
+	slog.Info("Waiting for messages from client", "client", conn.RemoteAddr().String())
 	for {
 		buffer := make([]byte, 6000)
 		n, err := conn.Read(buffer)
 		if err != nil {
-			// log here
-			// better error checking here
 			continue
 		}
 
-		fmt.Println("from client:", string(buffer))
 		cipherText, err := base64.StdEncoding.WithPadding(base64.StdPadding).DecodeString(string(buffer[:n]))
 		if err != nil {
 			panic(err)
@@ -92,23 +120,30 @@ func (server *GameServer) HandleClient(conn net.Conn) {
 func (server *GameServer) NegotiateKeys(conn net.Conn) {
 	negotiationSuccess := false
 
+	slog.Info("Starting key negotiation with client", "client", conn.RemoteAddr().String())
 	buffer := make([]byte, 4096)
 	_, err := conn.Read(buffer)
 	if err != nil {
-		panic(err) // transmission error during key negotation
+		slog.Error("Failed to read buffer from client during key negotiation", "client", conn.RemoteAddr().String())
+		conn.Close()
+		return
 	}
 
 	bufferStr := string(buffer)
 	if strings.HasPrefix(bufferStr, "JOIN:") {
+		slog.Info("Received JOIN request from client", "client", conn.RemoteAddr().String())
 		keyResp := "PUBKEY:" + string(crypto.PublicKeyToPEM(server.publicKey))
 		_, err := conn.Write([]byte(keyResp))
 		if err != nil {
-			panic(err) // transmission error during key negotiation
+			slog.Error("Failed to send key to client", "client", conn.RemoteAddr().String())
+			conn.Close()
+			return
 		}
 		negotiationSuccess = true
 	}
 
 	if negotiationSuccess {
+		slog.Info("Key negotiation success for client", "client", conn.RemoteAddr().String())
 		server.HandleClient(conn)
 	} else {
 		conn.Close()
