d7a48efd39a36a624b65d4296d27294df6d8c1b5
Steven A. Zaluk <stevezaluk@gmail.com>
2024-11-11 00:06:31 -0500

    Implemented errors for server side, key negotiation, and decryption errors

--- errors/errors.go
+++ errors/errors.go
@@ -1,2 +1,19 @@
 package errors
 
+import "errors"
+
+// Server Errors
+var ErrServerStartFailed = errors.New("server: Failed to start listening for connections")
+var ErrReadBufferFailed = errors.New("server: Failed to read buffer from client")
+var ErrWriteBufferFailed = errors.New("server: Failed to write buffer to client")
+var ErrAcceptConnectionFailed = errors.New("server: Failed to accept client connection")
+var ErrCloseConnectionFailed = errors.New("server: Failed to close connect from client")
+var ErrMaxConnectionsReached = errors.New("server: Cannot accept client connection. Server is full")
+
+// Key Negotiation
+var ErrInvalidConnectResponse = errors.New("keyNeogtiation: Did not receive expected CONNECT request from client during key negotiation")
+
+// Decryption
+var ErrKeysNotValid = errors.New("crypto: The keys generated for the server are not valid")
+var ErrBase64DecodeFailed = errors.New("crypto: Failed to base64 decode encrypted message sent from client")
+var ErrDecryptionFailed = errors.New("crypto: Failed to decrypt message from client")
