db80cd49624c29e689cd2a2800699b5aea40c722
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-19 14:23:21 -0500

    Added ZoneIds and had this replace ZoneType

--- game/zone.go
+++ game/zone.go
@@ -1,6 +1,15 @@
 package game
 
+const (
+	BattlefieldZoneId = "zone:battlefield"
+	ExileZoneId       = "zone:exile"
+	GraveyardZoneId   = "zone:graveyard"
+	HandZoneId        = "zone:hand"
+	DeckZoneId        = "zone:deck"
+	CommanderZoneId   = "zone:commander"
+)
+
 type Zone struct {
-	ZoneType string
-	Cards    []*CardObject
+	ZoneId string
+	Cards  []*CardObject
 }
