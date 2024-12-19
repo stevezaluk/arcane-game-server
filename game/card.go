6a228bd2b544cec2bb1981984ac7cb0fd4543867
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-19 14:09:23 -0500

    Defined the game object and zone object

---
+++ game/card.go
@@ -0,0 +1,33 @@
+package game
+
+import (
+	"github.com/stevezaluk/mtgjson-models/card"
+	"github.com/stevezaluk/mtgjson-models/user"
+)
+
+/*
+CardObject Represents the card played or generated for the game. This needs to differ
+from the protobuf models as there are additional values that need to be tracked like
+ownership, its parent zone, and the state of the card
+*/
+type CardObject struct {
+	Metadata   *card.CardSet
+	Owner      *user.User
+	ParentZone *Zone
+
+	IsTapped          bool
+	IsFaceDown        bool
+	WasPlayedThisTurn bool
+}
+
+/*
+NewCardObject Create a new pointer to a card object. Its Metadata, Owner, and ParentZone are required
+*/
+func NewCardObject(metadata *card.CardSet, owner *user.User, zone *Zone) *CardObject {
+	return &CardObject{
+		Metadata:          metadata,
+		Owner:             owner,
+		ParentZone:        zone,
+		WasPlayedThisTurn: true,
+	}
+}
---
+++ game/zone.go
@@ -0,0 +1,6 @@
+package game
+
+type Zone struct {
+	ZoneType string
+	Cards    []*CardObject
+}
