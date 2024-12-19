b71290d9a596026c3dbe2bba043ed6f52d1ffe75
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-19 18:25:55 -0500

    Defined the Player object

--- game/player.go
+++ game/player.go
@@ -1,1 +1,24 @@
 package game
+
+import (
+	"github.com/stevezaluk/mtgjson-models/user"
+	"net"
+)
+
+type Player struct {
+	User *user.User
+	Conn *net.Conn
+
+	Library   *DeckObject
+	Graveyard *Zone
+	Hand      *Zone
+
+	LifeTotal          int
+	CommanderDamage    int
+	PoisonCounters     int
+	EnergyCounters     int
+	ExperienceCounters int
+
+	IsMonarch   bool
+	IsGameOwner bool
+}
