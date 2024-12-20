1af1d2f74cb1d6d8057cf8c71ad1dbb4d7670576
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-19 21:39:03 -0500

    Defined the structure of the game object

--- game/game.go
+++ game/game.go
@@ -1,1 +1,21 @@
 package game
+
+const (
+	CommanderGameMode = "gamemode:commander"
+	ModernGameMode    = "gamemode:modern"
+	StandardGameMode  = "gamemode:standard"
+)
+
+/*
+Game A representation of a single MTG Game. Process's game commands sent from the client
+*/
+type Game struct {
+	Name     string
+	GameMode string
+
+	Players []*Player // ordered
+
+	Battlefield *Zone
+	Exile       *Zone
+	Command     *Zone
+}
