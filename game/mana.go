bf98db516b004d5e5e7e7687587ef9d796bca479
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-19 21:53:04 -0500

    Defined the structure for mana

---
+++ game/mana.go
@@ -0,0 +1,19 @@
+package game
+
+const (
+	GreenMana     = "mana:green"
+	BlueMana      = "mana:blue"
+	RedMana       = "mana:red"
+	WhiteMana     = "mana:white"
+	BlackMana     = "mana:black"
+	ColorlessMana = "mana:colorless"
+)
+
+/*
+Mana Abstraction of a in-game mana. The count indicates the number of mana we have for an individual color,
+this ensures that we don't have to do additional operations on the players mana pool
+*/
+type Mana struct {
+	Color string
+	Count int
+}
